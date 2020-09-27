ef1e10a48355f15a469def18efc1175953a78326
Jackson <jacksonhopknins92@gmail.com>
2020-09-26 22:35:53 -0400

    updated to be random

--- app/src/main/java/com/project/randomcats/data/vo/CatPictureDetails.kt
+++ app/src/main/java/com/project/randomcats/data/vo/CatPictureDetails.kt
@@ -4,12 +4,6 @@ package com.project.randomcats.data.vo
 import com.google.gson.annotations.SerializedName
 
 data class CatPictureDetails(
-    @SerializedName("height")
-    val height: Int,
-    @SerializedName("id")
-    val id: String,
-    @SerializedName("url")
-    val url: String,
-    @SerializedName("width")
-    val width: Int
+    @SerializedName("file")
+    val `file`: String
 )
